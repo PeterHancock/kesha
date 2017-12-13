163b9a8d4c80b0890a7b0d10ae7ff84d96aac69f
Peter Hancock <peter.hancock@ocado.com>
2017-12-13 18:09:45 +0000

    server

--- packages/server/__tests__/index.test.js
+++ packages/server/__tests__/index.test.js
@@ -1,24 +1,105 @@
 /* eslint-env jest */
+const express = require('express')
 
-const server = require('../')
+const dbServer = require('../')
 
-const db = new Array(1000).fill(0).reduce((acc, _, i) => {
+const request = require('request')
+
+const r = (...$) =>
+  new Promise((resolve, reject) => {
+    request(...$, (err, ...rest) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(...rest)
+      }
+    })
+  })
+
+const kesha = require('kesha')
+
+const blob = `123456789
+123456789
+123456789
+123456789
+123456789
+123456789
+123456789
+123456789
+123456789
+123456789
+`
+
+const initialStore = new Array(100).fill(0).reduce((acc, _, i) => {
   const key = `key-${i}`
-  acc[key] = { key, n: 1 }
+  acc[key] = { id: key, val: blob }
   return acc
 }, {})
 
-describe('kesha', () => {
-  const contentAddresed = kesha.addressContent(db)
-  test('addressContent', () => {
-    expect(contentAddresed).toMatchSnapshot()
-  })
-  const partition = kesha._partition(contentAddresed)
-  test('_partition', () => {
-    expect(partition).toMatchSnapshot()
-  })
-  const merkle = kesha._createMerkle(partition)
-  test('_createMerkle', () => {
-    expect(merkle).toMatchSnapshot()
+const keyToChange = 'key-10'
+const nextStore = Object.assign({}, initialStore, {
+  [keyToChange]: { id: keyToChange, val: 'suprise' },
+})
+const db = kesha.create(nextStore)
+
+describe('server', () => {
+  test('integration', done => {
+    const app = express()
+    app.use('/', dbServer())
+    const server = app.listen(9090, () => {
+      r({
+        url: 'http://localhost:9090/1',
+        method: 'POST',
+        json: true,
+        body: initialStore,
+      })
+        .then(() =>
+          r('http://localhost:9090/').then(({ body }) => {
+            expect(body).toMatchSnapshot()
+          })
+        )
+        .then(() =>
+          r({
+            url: 'http://localhost:9090/1/delta',
+            method: 'GET',
+            json: true,
+            body: db.merkle,
+          })
+        )
+        .then(response => {
+          const patched = kesha.patch(db, response.body)
+          return patched
+        })
+        .then(patched => {
+          r({
+            url: 'http://localhost:9090/1/version',
+            method: 'GET',
+            json: true,
+          }).then(response => {
+            const changedStore = Object.assign({}, patched.store, { 'key-0': 'HELLO' })
+            const delta = kesha.diff(kesha.create(changedStore), patched)
+            r({
+              url: 'http://localhost:9090/1',
+              method: 'PUT',
+              json: true,
+              body: { hash: response.body, delta },
+            }).then(() => {
+              r({
+                url: 'http://localhost:9090/1/version',
+                method: 'GET',
+                json: true,
+              }).then(response => {
+                expect(response.body).toBe(delta.merkle.hash)
+                console.error('closing server')
+                server.close(done)
+              })
+            })
+          })
+        })
+        .catch(() => {
+          console.error('closing server')
+          server.close(done)
+        })
+    })
   })
 })
--- packages/server/index.js
+++ packages/server/index.js
@@ -2,38 +2,60 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const kesha = require('kesha')
 
-const app = express()
-app.use('/', (req, res, next) => {
-  console.error('Request received:', req.url)
-  next()
-})
-app.use('/', bodyParser.json())
+module.exports = () => {
+  const app = express.Router()
+  app.use('/', (req, res, next) => {
+    console.error('Request received:', req.url, req.method)
+    next()
+  })
+  app.use('/', bodyParser.json())
 
-const stores = {}
+  const stores = {}
 
-app.get('/', (req, res) => {
-  const id = req.params.id
-  res.json(Object.keys(stores))
-})
+  app.get('/', (req, res) => {
+    const id = req.params.id
+    res.json(Object.keys(stores))
+  })
 
-app.post('/:id', (req, res) => {
-  const id = req.params.id
-  const store = req.body
-  stores[id] = {
-    store,
-    kesha: kesha.create(store),
-  }
-  res.end()
-})
+  app.post('/:id', (req, res) => {
+    const id = req.params.id
+    const store = req.body
+    const { cas, merkle } = kesha.create(store)
+    stores[id] = {
+      store,
+      cas,
+      merkle,
+    }
+    res.end()
+  })
 
-app.get('/:id/diff', (req, res) => {
-  const id = req.params.id
-  res.json(kesha.diff(req.body, stores[id].kesha))
-})
+  app.put('/:id', (req, res) => {
+    const id = req.params.id
+    const { hash, delta } = req.body
+    if (!(stores[id] && stores[id].merkle.hash === hash)) {
+      return res.status(409).end()
+    }
+    stores[id] = kesha.patch(stores[id], delta)
+    res.end()
+  })
 
-app.get('/:id', (req, res) => {
-  const id = req.params.id
-  res.json(stores[id].store)
-})
+  app.get('/:id/delta', (req, res) => {
+    const id = req.params.id
+    if (!stores[id]) res.status(404).end()
+    res.json(kesha.diff(stores[id], req.body))
+  })
 
-app.listen(9090)
+  app.get('/:id', (req, res) => {
+    const id = req.params.id
+    if (!stores[id]) res.status(404).end()
+    res.json(stores[id].store)
+  })
+
+  app.get('/:id/version', (req, res) => {
+    const id = req.params.id
+    if (!stores[id]) res.status(404).end()
+    res.json(stores[id].merkle.hash)
+  })
+
+  return app
+}
